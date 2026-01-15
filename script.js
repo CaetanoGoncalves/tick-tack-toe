3cd1e8dbe3daed597e065a7c360e357ba8c3f6d1
Caetano AG <caetanogoncalves.alves@gmail.com>
2026-01-15 15:47:48 -0300

    Melhoria na qualidade do código

--- script.js
+++ script.js
@@ -29,125 +29,13 @@ const buttonStates =
 document.getElementById("reset").addEventListener("click", (e) => {
     resetButtons();
 })
-buttonGrid[0][0].addEventListener('click', (e) => {
-    const retorno = value(jogador,0,0);
-    buttonGrid[0][0].innerHTML = retorno.button;
-    jogador = retorno.player;
-    if(findWinner() == 1)
+for(let i = 0; i < 3; i++)
+{
+    for(let j = 0; j < 3; j++)
     {
-        xWin();
+        buttonGrid[i][j].addEventListener("click", handleMove);
     }
-     if(findWinner() == 2)
-    {
-        ballWin()
-    }
-});
-buttonGrid[0][1].addEventListener('click', (e) => {
-    const retorno = value(jogador,0,1);
-    buttonGrid[0][1].innerHTML = retorno.button;
-    jogador = retorno.player;
-    if(findWinner() == 1)
-    {
-        xWin();
-    }
-     if(findWinner() == 2)
-    {
-        ballWin()
-    }
-});
-buttonGrid[0][2].addEventListener('click', (e) => {
-    const retorno = value(jogador,0,2);
-    buttonGrid[0][2].innerHTML = retorno.button;
-    jogador = retorno.player;
-    if(findWinner() == 1)
-    {
-        xWin();
-    }
-     if(findWinner() == 2)
-    {
-        ballWin()
-    }
-});
-
-buttonGrid[1][0].addEventListener('click', (e) => {
-    const retorno = value(jogador,1,0);
-    buttonGrid[1][0].innerHTML = retorno.button;
-    jogador = retorno.player;
-    if(findWinner() == 1)
-    {
-        xWin();
-    }
-     if(findWinner() == 2)
-    {
-        ballWin()
-    }
-});
-buttonGrid[1][1].addEventListener('click', (e) => {
-    const retorno = value(jogador,1,1);
-    buttonGrid[1][1].innerHTML = retorno.button;
-    jogador = retorno.player;
-    if(findWinner() == 1)
-    {
-        xWin();
-    }
-     if(findWinner() == 2)
-    {
-        ballWin()
-    }
-});
-buttonGrid[1][2].addEventListener('click', (e) => {
-    const retorno = value(jogador,1,2);
-    buttonGrid[1][2].innerHTML = retorno.button;
-    jogador = retorno.player;
-    if(findWinner() == 1)
-    {
-        xWin();
-    }
-     if(findWinner() == 2)
-    {
-        ballWin()
-    }
-});
-
-buttonGrid[2][0].addEventListener('click', (e) => {
-    const retorno = value(jogador,2,0);
-    buttonGrid[2][0].innerHTML = retorno.button;
-    jogador = retorno.player;
-    if(findWinner() == 1)
-    {
-        xWin();
-    }
-    if(findWinner() == 2)
-    {
-        ballWin()
-    }
-});
-buttonGrid[2][1].addEventListener('click', (e) => {
-    const retorno = value(jogador,2,1);
-    buttonGrid[2][1].innerHTML = retorno.button;
-    jogador = retorno.player;
-    if(findWinner() == 1)
-    {
-        xWin();
-    }
-     if(findWinner() == 2)
-    {
-        ballWin()
-    }
-});
-buttonGrid[2][2].addEventListener('click', (e) => {
-    const retorno = value(jogador,2,2);
-    buttonGrid[2][2].innerHTML = retorno.button;
-    jogador = retorno.player;
-    if(findWinner() == 1)
-    {
-        xWin();
-    }
-    if(findWinner() == 2)
-    {
-        ballWin()
-    }
-});
+}
 
 
 //================================
@@ -244,4 +132,23 @@ function findWinner()
         return v;
     }
     return 0;
+}
+function handleMove(e) {
+    const button = e.target;
+    const x = Number(button.dataset.x);
+    const y = Number(button.dataset.y);
+    const retorno = value(jogador, x, y);
+    button.innerHTML = retorno.button;
+    jogador = retorno.player;
+
+    const winner = findWinner();
+
+    if(winner === 1)
+    {
+        xWin();
+    }
+    if(winner === 2)
+    {
+        ballWin();
+    }
 }
